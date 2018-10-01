1298f77edc4b0f8c8c2fa29ad2fa91efe9a5d546
Rahib <rahib.rustemzade@gmail.com>
2018-10-01 17:39:22 +0400

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,17 +6,30 @@ $('.main').height(windowHeight)
 
 
 
-$('.white').sortable({
-    connectWith: '.white',
+$('.black').sortable({
+    connectWith: '.black',
     update: function(event, ui) {
       var changedList = this.id;
       var order = $(this).sortable('toArray');
       var positions = order.join(';');
   
-      console.log({
-        id: changedList,
-        positions: positions
-      });
+     
     }
   });
+
+
+
+
+
+  $('#blackBtn').click(function(){
+      let x = Math.floor((Math.random() * 6) + 1);
+      let y = Math.floor((Math.random() * 6) + 1);
+
+      console.log(x,y)
+  
+  })
+   
+  
+  
+  
  
