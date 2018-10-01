0de4d836748e88d976a2cc170cef04314a9a91fe
Rahib <rahib.rustemzade@gmail.com>
2018-10-02 00:09:25 +0400

    fourth

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,15 +21,53 @@ $('.black').sortable({
 
 
 
-  $('#blackBtn').click(function(){
-      let x = Math.floor((Math.random() * 6) + 1);
-      let y = Math.floor((Math.random() * 6) + 1);
+  function random(){
+     
+    $('.cube1').addClass('cube1Rotate');
+    $('.cube2').addClass('cube2Rotate');
+    
+      setTimeout(function() {
+        starting()
+        $('.cube1').removeClass('cube1Rotate');
+        $('.cube2').removeClass('cube2Rotate');
+     }, 2000);
+    
 
-      console.log(x,y)
+
+    
+
   
-  })
    
   
+  }
+
+   
+  function starting(){
+    
+   
+  let x = Math.floor((Math.random() * 6) + 1);
+  let y = Math.floor((Math.random() * 6) + 1);
+
+
+
+  //console.log(x,y)
+    if(x == '6'){ $('.cube1').css({transform: 'rotateY(' + 90 + 'deg)'})}
+    if(x == '5'){$('.cube1').css({transform: 'rotateX(' + -90 + 'deg)'})}
+    if(x == '3'){ $('.cube1').css({transform: 'rotateX(' + 180 + 'deg)'})}
+    if(x == '2'){ $('.cube1').css({transform: 'rotateX(' + 90 + 'deg)'})}
+    if(x == '1'){$('.cube1').css({transform: 'rotateY(' + -90 + 'deg)'})}
+      
+    
+    if(y == '6'){ $('.cube2').css({transform: 'rotateY(' + 90 + 'deg)'})}
+    if(y == '5'){$('.cube2').css({transform: 'rotateX(' + -90 + 'deg)'})}
+    if(y == '3'){ $('.cube2').css({transform: 'rotateX(' + 180 + 'deg)'})}
+    if(y == '2'){ $('.cube2').css({transform: 'rotateX(' + 90 + 'deg)'})}
+    if(y == '1'){$('.cube2').css({transform: 'rotateY(' + -90 + 'deg)'})}
+      
+
+  }
+ 
   
-  
+
+
  
